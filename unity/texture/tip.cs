9dda2df7690e3ab3311fbd4fb7d3c7621b082546
yz <lightlfyan@gmail.com>
2015-03-09 11:34:41 +0800

    tip head

---
+++ unity/texture/tip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tip : MonoBehaviour
+{
+
+    public GameObject target;
+    
+    RectTransform rtf;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rtf = GetComponent<RectTransform> ();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        rtf.anchoredPosition = RectTransformUtility.WorldToScreenPoint (Camera.main, target.transform.position);
+    }
+}
